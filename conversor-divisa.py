33399ffc469f8cfadb192b8297ba5474ea18b166
limola23 <molanolili23@gmail.com>
2023-03-19 22:07:38 -0500

    segundo proyecto python

---
+++ conversor-divisa.py
@@ -0,0 +1,4 @@
+print("Bienvenido a mi conversor de GBP a EUR")
+valor_en_GBP = float (input("Introduzca el valor en GBP a convertir en EUR: "))
+conversion_GBP_EUR = float (input("Introduzca la tasa de conversión EUR GBP: "))
+print ("La cantidad en GBP es: {}" .format(valor_en_GBP * conversion_GBP_EUR))
